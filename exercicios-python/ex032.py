36bfccb11493443eddf1c832ae2dbe6d96996301
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-25 15:02:15 -0300

    Update ex032.py

--- exercicios-python/ex032.py
+++ exercicios-python/ex032.py
@@ -4,6 +4,6 @@ ano = int(input('Digite um ano qualquer (0 para calcular o ano da minha máquina
 if ano == 0:
     ano = date.today().year
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
-    print(f'ANO BISSEXTO ({ano})')
+    print(f'ANO BISSEXTO (\033[1;36m{ano}\033[m)')
 else:
-    print(f'{ano} NÃO É BISSEXTO')
+    print(f'\033[1;35;42m{ano}\033[m NÃO É BISSEXTO')
