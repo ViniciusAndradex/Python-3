513cb46f2b0c7eef0b6ca9d89ed224124280e21a
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-25 17:27:41 -0300

    Update ex042.py

--- exercicios-python/ex042.py
+++ exercicios-python/ex042.py
@@ -5,7 +5,7 @@ if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print('É POSSÍVEL FORMAR UM TRIÂNGULO!')
     if r1 == r2 == r3:
         print('\033[1;36mCATEGORIA: EQUILÁTERO\033[m')
-    elif r1 != r2 != r3:
+    elif r1 != r2 != r3 != r1:
         print('\033[1;32mCATEGORIA: ESCALENO\033[m')
     else:
         print('\033[1;37;42mCATEGORIA: ISÓSCELES\033[m')
