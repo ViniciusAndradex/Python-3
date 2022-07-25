e49223f070c3ecdf672a3a60be8487f3e3f33aeb
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-25 16:05:53 -0300

    Create ex038.py

---
+++ exercicios-python/ex038.py
@@ -0,0 +1,8 @@
+n = int(input('Digite um número: '))
+n1 = int(input('Digite outro número: '))
+if n > n1:
+    print('O primeiro é maior!!')
+elif n1 > n:
+    print('O segundo é maior!!')
+else:
+    print('O números são iguais!!')
