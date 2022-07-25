fa26735945d8fa9ee4684a4e3cee9270574e88fe
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-25 13:27:54 -0300

    Update ex032.py

--- exercicios-python/ex032.py
+++ exercicios-python/ex032.py
@@ -1,2 +1,5 @@
 ano = int(input('Digite um ano qualquer: '))
-if ano % 4 == 0 & ano % 100 != 0:
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
+    print(f'ANO BISSEXTO ({ano})')
+else:
+    print(f'{ano} NÃO É BISSEXTO')
