5108622ba26d7fd09ee535bc113c3c012becbeda
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-25 17:42:04 -0300

    Update ex045.py

--- exercicios-python/ex045.py
+++ exercicios-python/ex045.py
@@ -9,30 +9,46 @@ jogador = int(input('''\033[1;34m[1] Pedra
 [2] Papel
 [3] Tesoura
 \033[1;31m- '''))
+print('Jo')
+sleep(1)
+print('Ken')
+sleep(1)
+print('Po')
+sleep(1)
 bot = randint(1, 3)
 print('\033[1;35mPROCESSANDO...')
-sleep(2)
+sleep(1)
+print('-*-' * 6)
+print(f'\033[1;32mVoce jogou {jogos[jogador]}\nO bot jogou {jogos[bot]}')
+print('\033[1;35m-*-' * 6)
+sleep(1)
 if jogador == 1:
     if bot == 1:
         print('\033[1;33mDRAW')
     elif bot == 2:
-        print(f'\033[1;36mYOU LOSE, bot {jogos[bot]}')
+        print(f'\033[1;36mYOU LOSE')
+    elif bot == 3:
+        print(f'\033[1;34mYOU WIN')
     else:
-        print(f'\033[1;34mYOU WIN, bot {jogos[bot]}')
+        print('INVALIDO')
 elif jogador == 2:
     if bot == 1:
-        print(f'\033[1;34mYOU WIN, bot {jogos[bot]}')
+        print(f'\033[1;34mYOU WIN')
     elif bot == 2:
         print('\033[1;33mDRAW')
+    elif bot == 3:
+        print(f'\033[1;36mYOU LOSE')
     else:
-        print(f'\033[1;36mYOU LOSE, bot {jogos[bot]}')
+        print('INVALIDO')
 elif jogador == 3:
     if bot == 1:
-        print(f'\033[1;36mYOU LOSE, bot {jogos[bot]}')
+        print(f'\033[1;36mYOU LOSE')
     elif bot == 2:
-        print(f'\033[1;34mYOU WIN, bot {jogos[bot]}')
-    else:
+        print(f'\033[1;34mYOU WIN')
+    elif bot == 3:
         print('\033[1;33mDRAW')
+    else:
+        print('INVALIDO')
 else:
     print('OPÇÃO INVALIDA')
 print('\033[1;35;40mTHANK YOU FOR PLAY MY GAME!\033[m')
