a7082f64bacf42ba4776072cc751a7d58f612a3f
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-25 13:53:37 -0300

    Create ex033.py

---
+++ exercicios-python/ex033.py
@@ -0,0 +1,18 @@
+n = int(input('Digite um numero: '))
+n1 = int(input('Digite outro numero: '))
+n2 = int(input('Digite outro numero: '))
+menor = n
+if n2 > n1 < n:
+    menor = n1
+if n1 > n < n2:
+    menor = n
+if n > n2 < n1:
+    menor = n2
+maior = n
+if n < n1 > n2:
+    maior = n1
+if n1 < n > n2:
+    maior = n
+if n < n2 > n1:
+    maior = n2
+print(f'Maior: {maior}\nMenor: {menor}')
