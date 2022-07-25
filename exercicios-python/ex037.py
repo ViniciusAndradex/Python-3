2abc7adad32d64003e3d642b0b6d0c76835355d0
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-25 16:03:50 -0300

    Create ex037.py

---
+++ exercicios-python/ex037.py
@@ -0,0 +1,12 @@
+decimal = int(input('Digite um número: '))
+menu = int(input('''\033[1;35mCONVERSOR DE BASES NUMERICAS\033[m
+\033[1;31m[1] BINÁRIO
+[2] OCTAL
+[3] HEXADECIMAL
+\033[m- \033[1;34m'''))
+if menu == 1:
+    print(f'{decimal} -> BINARIO: {bin(decimal)[2:]}')
+elif menu == 2:
+    print(f'{decimal} -> OCTAL: {oct(decimal)[2:]}')
+else:
+    print(f'{decimal} -> HEXADECIMAL: {hex(decimal)[2:]}')
