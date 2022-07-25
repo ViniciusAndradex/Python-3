c807d0335ec1f3ce76ecc09d7160c23fb63eb324
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-25 09:53:52 -0300

    add and update

---
+++ exemplos-aula/Aula_09_teste1.py
@@ -0,0 +1,26 @@
+frase = 'Curso em Video Python'
+print(frase)
+print(frase[3:13])
+print(frase[:13])
+print(frase[1:15])
+print(frase[1:15:2])
+print(frase[1::2])
+print(frase[::2])
+print(frase.count('O'))
+print(frase.count('o')) #Case sensitive
+print(frase.upper().count('O'))
+print(len(frase))
+
+frase = ('   Curos em Video Python   ')
+print(frase.strip())
+print('-'.join(frase))
+print(frase.replace('Python', 'Android'))
+print(frase.split())
+
+frase = 'Curso em Video Python'
+print('Curso' in frase)
+print(frase.find('Curso'))
+print(frase.find('curso'))
+print(frase.lower().find('curso'))
+div = frase.split()
+print(div[2][3])
