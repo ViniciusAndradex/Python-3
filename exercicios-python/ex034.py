29b66e1a92ddcd7f9163fc3942f3a4ef83add03d
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-25 14:59:11 -0300

    Update ex034.py

--- exercicios-python/ex034.py
+++ exercicios-python/ex034.py
@@ -3,4 +3,4 @@ if salario > 1250.00:
     newSalario = (salario * 10 / 100) +salario
 else:
     newSalario = (salario * 15 / 100) + salario
-print(f'Seu novo salário é R${newSalario:.2f}')
+print(f'Seu novo salário é \033[4;35;40mR${newSalario:.2f}\033[m')
