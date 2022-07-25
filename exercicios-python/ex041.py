4e8b1d082b4a6ff6a4163e4010e598aacbbbf111
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-25 16:20:30 -0300

    Create ex041.py

---
+++ exercicios-python/ex041.py
@@ -0,0 +1,15 @@
+from datetime import date
+
+anoNascimento = int(input('Digite seu ano de nascimento: '))
+idade = date.today().year - anoNascimento
+print(f'IDADE: {idade}')
+if idade <= 9:
+    print('\033[1;31mCATEGORIA: MIRIM')
+elif idade <= 14:
+    print('\033[1;32mCATEGORIA: INFANTIL')
+elif idade <= 19:
+    print('\033[1;33mCATEGORIA: JUNIOR')
+elif idade == 20:
+    print('\033[1;34mCATEGORIA: SÊNIOR')
+else:
+    print('\033[1;35mCATEGORIA: MASTER\033[m')
