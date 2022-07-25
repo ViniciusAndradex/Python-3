cf57447cda446d8f07df2a57e1c2c3bd15100944
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-25 17:16:41 -0300

    Update ex039.py

--- exercicios-python/ex039.py
+++ exercicios-python/ex039.py
@@ -3,10 +3,13 @@ from datetime import date
 anoNascimento = int(input('Ano de nascimento: '))
 idade = date.today().year - anoNascimento
 if idade < 18:
-    falta = 18 - idade
-    print(f'Ainda faltam {falta} anos para se alistar!')
+    saldo = 18 - idade
+    print(f'Ainda faltam {saldo} anos para se alistar!')
+    print(f'Ano de alistamento: {date.today().year + saldo}')
 elif idade > 18:
-    passou = idade - 18
-    print(f'Já passaram {passou} anos do seu alistamento!')
+    saldo = idade - 18
+    print(f'Já passaram {saldo} anos do seu alistamento!')
+    print(f'Ano de alistamento: {date.today().year - saldo}')
 else:
     print(f'\033[1;35;42mVocê possui a idade do alistamento, {idade} anos. CUIDA!\033[m')
+    print(f'Ano de alistamento: {date.today().year}')
