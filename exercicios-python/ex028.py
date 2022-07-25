57004ef83017674e5798d97096f6282633effc3c
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-25 13:56:24 -0300

    Update ex028.py

--- exercicios-python/ex028.py
+++ exercicios-python/ex028.py
@@ -1,7 +1,10 @@
 from random import randint
+from time import sleep
 
 jogada = int(input('Adivinho o número do bot [0/5]: '))
 bot = randint(0, 5)
+print('Processando...')
+sleep(1)
 if jogada == bot:
     print('Você venceu parabéns')
 else:
