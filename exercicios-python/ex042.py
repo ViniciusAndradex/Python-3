636ba15000166ad23a64f06f2349cec95b142ab3
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-25 16:28:58 -0300

    Create ex042.py

---
+++ exercicios-python/ex042.py
@@ -0,0 +1,13 @@
+r1 = int(input('Digite o primeiro segmento de reta: '))
+r2 = int(input('Digite o segundo segmento de reta: '))
+r3 = int(input('Digite o terceiro segmento de reta: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('É POSSÍVEL FORMAR UM TRIÂNGULO!')
+    if r1 == r2 == r3:
+        print('\033[1;36mCATEGORIA: EQUILÁTERO\033[m')
+    elif r1 != r2 != r3:
+        print('\033[1;32mCATEGORIA: ESCALENO\033[m')
+    else:
+        print('\033[1;37;42mCATEGORIA: ISÓSCELES\033[m')
+else:
+    print('NÃO É POSSÍVEL FORMA TRIÂNGULO')
