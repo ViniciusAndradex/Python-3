2f36ababe2caadc857d2ec1b68e0a6cd22650947
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-27 11:54:04 -0300

    add

---
+++ exercicios-python/ex049.py
@@ -0,0 +1,3 @@
+num = int(input('Digite um número: '))
+for c in range(1, 10 + 1):
+    print(f'{num} x {c:2} = {num * c:3}')
---
+++ exercicios-python/ex050.py
@@ -0,0 +1,5 @@
+n = 0
+for c in range(1, 6 + 1):
+    if c % 2 == 0:
+        n += c
+print(f'A soma dos pares é {n}')
