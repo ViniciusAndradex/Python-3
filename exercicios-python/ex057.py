e2e86c8280c595fac3a19466ad40d1b59bb15fe5
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-27 16:15:58 -0300

    Update ex057.py

--- exercicios-python/ex057.py
+++ exercicios-python/ex057.py
@@ -1,4 +1,4 @@
-sexo = ''
-while sexo != 'F' and sexo != 'M':
-    sexo = str(input('Sexo [F/M]: ')).strip().upper()
-print(sexo)
+sexo = str(input('Sexo [F/M]: ')).strip().upper()[0]
+while sexo not in 'MF':
+    sexo = str(input('Dados invalidos. Digite seu sexo[F/M]: ')).strip().upper()[0]
+print(f'Sexo {sexo} registrado com sucesso.')
