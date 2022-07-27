d6b5a8d7eb15e204af65e2059014bb12e38e4d19
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-27 12:09:57 -0300

    Create ex052.py

---
+++ exercicios-python/ex052.py
@@ -0,0 +1,15 @@
+primo = int(input('Digite um numero: '))
+somaPrimo = 0
+print('DIVISIVEIS ESTÃO COLORIDOS: ')
+for c in range(1, primo + 1):
+    if primo % c == 0:
+        print(f'\033[1;34m{c}\033[m', end=' ')
+        somaPrimo += 1
+    else:
+        print(f'{c}', end=' ')
+    if c == primo:
+        print()
+if somaPrimo > 2:
+    print(f'\033[4;33m{primo}\033[m não é primo!')
+else:
+    print(f'\033[4;32m{primo}\033[m é primo!')
