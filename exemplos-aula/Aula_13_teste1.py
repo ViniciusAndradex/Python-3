b4fff480875f719b1a5a579f10b40dde28782f0b
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-27 10:34:09 -0300

    add and update

--- exemplos-aula/Aula_13_teste1.py
+++ exemplos-aula/Aula_13_teste1.py
@@ -1,1 +1,21 @@
-print('oi')
+print('-' * 15)
+for c in range(1, 7):
+    print(c)
+print('FIM')
+print('-' * 15)
+for c in range(6, 0, -1):
+    print(c)
+print('Fim')
+print('-' * 15)
+ini = int(input('Digite um numero: '))
+fim = int(input('Digite um numero: '))
+passo = int(input('Digite um numero: '))
+for c in range(ini, fim + 1, passo):
+    print(c)
+print('-' * 15)
+s = 0
+for c in range(0, 3):
+    n = int(input('Digite um valor: '))
+    s += n
+print(f'Soma: {s}')
+print('-' * 15)
