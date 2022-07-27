c4e7f1e50f786d9692fdd3885d7acd13532a43b3
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-27 13:32:57 -0300

    Update ex050.py

--- exercicios-python/ex050.py
+++ exercicios-python/ex050.py
@@ -1,7 +1,8 @@
 acumulador = 0
 contador = 0
 for c in range(1, 6 + 1):
-    if c % 2 == 0:
+    n = int(input(f'Digite {c}° valor: '))
+    if n % 2 == 0:
         acumulador += c
         contador += 1
 print(f'A soma dos {contador} números pares é {acumulador}')
