a29dc24d890d59951e83d38d970e8ae988f8cbe1
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-27 16:47:56 -0300

    Update ex062.py

--- exercicios-python/ex062.py
+++ exercicios-python/ex062.py
@@ -2,13 +2,16 @@ pa = int(input('Primeiro termo: '))
 razao = int(input('Razão: '))
 termo = 1
 cont = 10
+num = 0
 while termo != 0:
     print(f'{pa + (termo - 1) * razao}', end=' -> ')
     termo += 1
     if termo == cont:
+        print('PAUSA')
         cont = int(input('\nDeseja exibir mais quantos termo (0 para sair)?\n - '))
         if cont == 0:
             termo = cont
         else:
             cont += termo
-print('FIM')
+            num = cont
+print(f'Foram mostrados {num} termos.')
