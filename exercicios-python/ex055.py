6cbd8e9d6cc9d0a3d202c80cb7d9ecba962b2e9d
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-27 14:00:19 -0300

    Update ex055.py

--- exercicios-python/ex055.py
+++ exercicios-python/ex055.py
@@ -3,8 +3,9 @@ for c in range(1, 6):
     peso = float(input('Digite seu peso: '))
     if c == 1:
         maior = menor = peso
-    elif maior < peso:
-        maior = peso
-    elif menor > peso:
-        menor = peso
+    else:
+        if maior < peso:
+            maior = peso
+        if menor > peso:
+            menor = peso
 print(f'Mais pesado: {maior}Kg\nMais leve: {menor}Kg')
