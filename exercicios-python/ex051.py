be5e301e9304e7b6e40842176b9b83dea90b7c1d
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-27 13:41:31 -0300

    Update ex051.py

--- exercicios-python/ex051.py
+++ exercicios-python/ex051.py
@@ -1,4 +1,7 @@
 primeiroTermo = int(input('Digite o primeiro termo da PA: '))
 razao = int(input('Digite a razão: '))
 for c in range(1, 11):
-    print(f'{c}° = {primeiroTermo + (c - 1) * razao}')
+    print(f'{c}° = {primeiroTermo + (c - 1) * razao}', end='-> ')
+print(' Fim!')
+
+# for c in range(primeiroTermo, primeiro termo + (10-1) * razao + razao, razao):
