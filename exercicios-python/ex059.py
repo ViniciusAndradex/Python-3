fae9fce9cfd7892d9ea6a930e0d839549de7840e
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-27 15:04:09 -0300

    Create ex059.py

---
+++ exercicios-python/ex059.py
@@ -0,0 +1,28 @@
+menu = 0
+n = int(input('Digite um valor: '))
+n1 = int(input('Digite outro valor: '))
+while menu != 5:
+    menu = int(input('''[1] Somar
+[2] Multiplicar
+[3] Maior
+[4] Novos números
+[5] Sair do programa
+- '''))
+    print('-*-' * 10)
+    if menu == 1:
+        print(f'{n} + {n1} = {n + n1}')
+    elif menu == 2:
+        print(f'{n} * {n1} = {n * n1}')
+    elif menu == 3:
+        if n > n1:
+            print(f'{n} é maior que {n1}')
+        else:
+            print(f'{n1} é maior que {n}')
+    elif menu == 4:
+        n = int(input('Digite um valor: '))
+        n1 = int(input('Digite outro valor: '))
+    elif menu == 5:
+        print('Thanks for using the app!')
+    else:
+        print('\033[1;31mOpção Inválida!!\033[m')
+    print('-*-' * 10)
