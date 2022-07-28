8fff905813409e6944684c8a12106661b1fef280
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-28 15:44:40 -0300

    add and update

---
+++ exercicios-python/ex074.py
@@ -0,0 +1,5 @@
+from random import randint
+tupla = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+print(tupla)
+print(f'Maior número: {max(tupla)}')
+print(f'Menor número: {min(tupla)}')
