ffc6c0b8697f4be562fed0df8f31c9496d063704
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-28 16:26:34 -0300

    add

---
+++ exercicios-python/ex075.py
@@ -0,0 +1,12 @@
+tupla = (int(input('Digite um número: ')), int(input('Digite um número: ')), int(input('Digite um número: ')),
+         int(input('Digite um número: ')))
+print(f'Valores digitados: {tupla}')
+print(f'Valor 9 foi digitado {tupla.count(9)} vezes.')
+if tupla.index(3) >= 0:
+    print(f'O número 3 apareceu na {tupla.index(3) + 1}° posição')
+else:
+    print('O número 3 não apareceu em nenhuma posição')
+print('Os valores pares digitados foram ', end='')
+for c in tupla:
+    if c % 2 == 0:
+        print(c, end=' ')
