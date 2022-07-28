34ec8b5bc1843824f530879b9219210190117810
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-28 15:08:44 -0300

    add Tuplas

---
+++ exemplos-aula/Aula_16_Teste1TUPLAS.py
@@ -0,0 +1,22 @@
+lanche = ('Hamburguer', 'Suco', 'Pizza', 'Pudim')
+print(lanche)
+print(lanche[1])
+print(lanche[::-1])
+print(lanche[-2])
+print(lanche[1:3])
+print(lanche[2:])
+print(lanche[:2])
+print(lanche[-2:])
+print('-' * 20)
+# Tuplas são imutaveis.
+# Traceback: lanche[1] = 'Refrigerante'
+print(len(lanche))
+
+for cont in range(0, len(lanche)):
+    print(f' 1 - Eu vou comer {lanche[cont]}, na posição {cont}')
+for c in lanche:
+    print(f'2 - Eu vou comer {c}')
+for pos, c in enumerate(lanche):
+    print(f'2 - Eu vou comer {c}, na posição {pos}')
+print('-' * 20)
+print(sorted(lanche))
---
+++ exemplos-aula/Aula_16_Teste2Tuplas.py
@@ -0,0 +1,12 @@
+a = (2, 5, 4)
+b = (5, 8, 1, 2)
+c = a + b
+print(c)
+print(c.count(5))
+print(c.index(8))
+print('-' * 35)
+pessoa = ('Gustavo', 39, 'M', 99.88)
+print(pessoa)
+print('-' * 35)
+del pessoa
+print(pessoa)
