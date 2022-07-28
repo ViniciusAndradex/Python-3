6ff36e909582c44efa81dce6a6baac74b8d33cf8
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-28 16:45:14 -0300

    add and update

--- exemplos-aula/Aula_16_Teste1TUPLAS.py
+++ exemplos-aula/Aula_16_Teste1TUPLAS.py
@@ -17,6 +17,6 @@ for cont in range(0, len(lanche)):
 for c in lanche:
     print(f'2 - Eu vou comer {c}')
 for pos, c in enumerate(lanche):
-    print(f'2 - Eu vou comer {c}, na posição {pos}')
+    print(f'3 - Eu vou comer {c}, na posição {pos}')
 print('-' * 20)
 print(sorted(lanche))
---
+++ exercicios-python/ex077.py
@@ -0,0 +1,8 @@
+tupla = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis', 'estudar', 'praticar', 'trabalhar',
+         'mercado', 'programador', 'futuro')
+for c in tupla:
+    print(f'Na palavra {c.upper()} temos ', end='')
+    for vogal in c:
+        if vogal in 'aeiou':
+            print(vogal[0], end=' ')
+    print()
