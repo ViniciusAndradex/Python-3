2c7d2ac19d1242e2b8e7b1197d068ed60d6eebe7
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-28 16:51:06 -0300

    Update ex073.py

--- exercicios-python/ex073.py
+++ exercicios-python/ex073.py
@@ -3,6 +3,6 @@ tupla = ('Palmeiras', 'Corinthians', 'Fluminense', 'Atlético-MG', 'Athletico-PR
          'Cuiabá', 'Atlético-GO', 'Juventude', 'Fortaleza')
 print(f'{" BRASILEIRÃO ":=^31}')
 print(f'TOP 5: {tupla[:5]}')
-print(f'Z4: {tupla[:-5:-1]}')
+print(f'Z4: {tupla[-4:]}')
 print(f'Times na Ordem alfabética: {sorted(tupla)}')
 print(f'Posição do Botafogo: {tupla.index("Botafogo") + 1}°')
