69bd9047bac9f9a29534d55359a6589ed756f8b0
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-29 16:06:11 -0300

    Update ex088.py

--- exercicios-python/ex088.py
+++ exercicios-python/ex088.py
@@ -3,14 +3,21 @@ from time import sleep
 sorteados = list()
 sorteio = list()
 numSorteio = int(input('Quantos jogos você quer que eu sorteie? '))
+cont = 0
 for num in range(0, numSorteio):
-    for jogos in range(0, 6):
-        sorteio.append(randint(1, 60))
+    while True:
+        x = randint(1, 60)
+        if x not in sorteio:
+            sorteio.append(x)
+            cont += 1
+        if cont >= 6:
+            cont = 0
+            break
     sorteados.append(sorteio[:])
     sorteio.clear()
 # Apresentação
 print(f'{f" SORTEANDO {numSorteio} JOGOS ":-^40}')
 for jogadas in range(0, numSorteio):
-    print(f'Jogo {jogadas + 1:2}: {sorteados[jogadas]}')
+    print(f'Jogo {jogadas + 1:2}: {sorted(sorteados[jogadas])}')
     sleep(0.5)
 print(f'{f" < BOA SORTE! > ":-^40}')
