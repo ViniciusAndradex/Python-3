9f8c69a0c97456365425cee4a4b97a4908e7bbdd
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-29 11:15:18 -0300

    Update ex080.py

--- exercicios-python/ex080.py
+++ exercicios-python/ex080.py
@@ -2,12 +2,13 @@ lista = list()
 for item in range(0, 5):
     num = int(input('Digite um número: '))
     if item == 0 or num > lista[-1]:
-        print('Adicionado ao final...')
         lista.append(num)
+        print('Adicionado ao final...')
+
     else:
         for pos, c in enumerate(lista):
             if c > num:
-                print(f'Adionado na posição {pos} da lista...')
                 lista.insert(pos, num)
+                print(f'Adionado na posição {pos} da lista...')
                 break
 print(lista)
