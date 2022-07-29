078823f1de7645d7965941c87d19ed40cdaa7c12
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-07-29 20:18:27 -0300

    Update ex090.py

--- exercicios-python/ex090.py
+++ exercicios-python/ex090.py
@@ -1,13 +1,12 @@
 alunos = dict()
 alunos['nome'] = str(input('Digite seu nome: '))
 alunos['media'] = float(input(f'Média de {alunos["nome"]}: '))
-print('-' * 15)
-print(f'- nome é igual a {alunos["nome"]}')
-print(f'- média é igual a {alunos["media"]}')
 if alunos['media'] >= 7.0:
     alunos['situacao'] = 'Aprovado'
 elif alunos['media'] >= 5.0:
     alunos['situacao'] = 'Recuperação'
 else:
     alunos['situacao'] = 'Reprovado'
-print(f'- situação é igual a {alunos["situacao"]}')
+print('-' * 15)
+for k, v in alunos.items():
+    print(f'- {k} é igual a {v}')
