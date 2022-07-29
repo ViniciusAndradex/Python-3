88381a027e24fd11dc73fdd5b0d107ed083cefe2
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-29 14:09:10 -0300

    Create ex086.py

---
+++ exercicios-python/ex086.py
@@ -0,0 +1,9 @@
+matriz = [[], [], []]
+for lin in range(0, 3):
+    for col in range(0, 3):
+        matriz[lin].append(int(input(f'Digite um valor para [{lin}, {col}]: ')))
+for p in matriz:
+    for d in range(0, len(p)):
+        print(f'[{p[d]:^5}]', end='')
+    print()
+    
