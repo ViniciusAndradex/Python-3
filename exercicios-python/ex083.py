759a3b38d82fe99bdd73c5869979ee06792ecf1d
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-07-29 11:25:09 -0300

    Update ex083.py

--- exercicios-python/ex083.py
+++ exercicios-python/ex083.py
@@ -1,6 +1,20 @@
 lista = list()
+#expr = str(input('Digite a expressão: ')), PREFIRO A MINHA Lógica.
 lista.append(input('Digite uma expressão: ').strip())
 if (lista[0].count('(') + lista[0].count(')')) % 2 == 0:
     print('Sua expressão está correta!')
 else:
     print('Sua expressão está errada!')
+#for simb in expr:
+#    if simb == '(':
+#        lista.append('(')
+#    elif simb == ')':
+#        if len(lista) > 0:
+#            lista.pop()
+#        else:
+#            lista.append(')')
+#            break
+#if len(lista) == 0:
+#    print('Sua expressão está válida!')
+#else:
+#    print('Sua expressão está errada!')
