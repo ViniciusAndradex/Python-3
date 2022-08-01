3ff32c32386739e40d5c3b91802ecd785be678be
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-08-01 17:55:08 -0300

    Update ex094.py

--- exercicios-python/ex094.py
+++ exercicios-python/ex094.py
@@ -7,6 +7,7 @@ while True:
         pessoa['sexo'] = str(input('Sexo: ')).strip().upper()[0]
         if pessoa['sexo'] in 'MF':
             break
+        print('ERRO! Por favor, digite apenas M ou F.')
     pessoa['idade'] = int(input('Idade: '))
     media += pessoa['idade']
     livrodeouro.append(pessoa.copy())
@@ -15,6 +16,7 @@ while True:
         resp = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
         if resp in 'SN':
             break
+        print('ERRO! Por favor, digite apenas S ou N.')
     if resp == 'N':
         break
 print('-*-' * 10)
