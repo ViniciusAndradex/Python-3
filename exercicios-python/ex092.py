741b71a0101d3a362c44303771346c9879f3467e
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-08-01 17:46:37 -0300

    update

--- exercicios-python/ex092.py
+++ exercicios-python/ex092.py
@@ -1,10 +1,11 @@
 from datetime import date
+#from datetime import datetime >>>> datetime.now().year
 trabalho = dict()
 trabalho['nome'] = str(input('Nome: ')).strip().capitalize()
 nasc = int(input('Data de Nascimento: '))
 trabalho['idade'] = date.today().year - nasc
 trabalho['ctps'] = int(input('Carteira de Trabalho (0 não tem): '))
-if trabalho['ctps'] > 0:
+if trabalho['ctps'] != 0:
     trabalho['contratação'] = int(input('Ano de Contratação: '))
     trabalho['salario'] = float(input('Salário: R$'))
     trabalho['aposentadoria'] = (trabalho['contratação'] + 35 - date.today().year) + trabalho['idade']
