b3d346692f46b8b95300fe47c957a1fbbfe2b61b
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-08-02 13:40:31 -0300

    Create ex100.py

---
+++ exercicios-python/ex100.py
@@ -0,0 +1,26 @@
+from random import randint
+from time import sleep
+
+
+def sorteio(lista):
+    print('Sorteando 5 valores da lista: ', end='')
+    for c in range(0, 5):
+        lst = randint(1, 10)
+        print(lst, end=' ')
+        sleep(0.5)
+        lista.append(lst)
+    print('PRONTO!')
+
+
+def somaPar(lst):
+    soma = 0
+    for c in lst:
+        if c % 2 == 0:
+            soma += c
+    print(f'Somando os valores pares de {lst}, temos {soma}')
+
+
+# Programa Principal:
+lista = list()
+sorteio(lista)
+somaPar(lista)
