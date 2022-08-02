40c6a0ac2eae307d48b784b4dc323335b512a437
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-08-02 15:33:06 -0300

    update

---
+++ exemplos-aula/Aula_21_teste1.py
@@ -0,0 +1,34 @@
+def fatorial(num=1):
+    f = 1
+    for c in range(num, 0, -1):
+        f *= c
+    return f
+
+
+def soma(a=0, b=0, c=0):
+    """
+    Faz a soma entre os Parâmetros
+    :param a: Valor Inteiro
+    :param b: Valor Inteiro
+    :param c: Valor Inteiro
+    :return: Sem Retorno
+    :author: Vinicius Andrade
+    """
+    s = a + b + c
+    print(s)
+
+
+def par(n=0):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+    
+
+soma(1, b=4)
+help(soma)
+print('-' * 10)
+n = int(input('Digite um número: '))
+print(f'O faotiral de {n} é {fatorial(n)}')
+print('-' * 10)
+print(par(9))
