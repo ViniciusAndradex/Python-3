6226342e035edb3ebf69d44bbc92e5dbfb78a23b
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-08-02 13:20:25 -0300

    Create ex099.py

---
+++ exercicios-python/ex099.py
@@ -0,0 +1,26 @@
+from time import sleep
+
+
+def lin():
+    print('-=-' * 20)
+
+
+def maior(* num):
+    lin()
+    print('Analisando os valores passados...')
+    for n in num:
+        print(n, end=' ')
+        sleep(0.5)
+    print(f'Foram informados {len(num)} valores ao todo.')
+    if len(num) == 0:
+        print(f'O maior valor informado foi 0.')
+    else:
+        print(f'O maior valor informado foi {max(num)}.')
+
+
+# Programa Principal
+maior(2, 9, 4, 5, 7, 1)
+maior(4, 7, 0)
+maior(1, 2)
+maior(6)
+maior()
