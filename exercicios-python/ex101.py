799c311f41c4e0346e7c3316da806b29269b2141
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-08-02 15:49:14 -0300

    Create ex101.py

---
+++ exercicios-python/ex101.py
@@ -0,0 +1,15 @@
+def voto(ano):
+    from datetime import datetime
+    idade = datetime.now().year - ano
+    if idade < 18:
+        opc = 'NÃO VOTA'
+    elif idade >= 65:
+        opc = 'VOTO OPCIONAL'
+    else:
+        opc = 'VOTO OBRIGATÓRIO'
+    return f'Com {idade} anos: {opc}'
+
+
+# Programa Principal
+print('-' * 20)
+print(voto(ano=int(input('Ano de Nascimento: '))))
