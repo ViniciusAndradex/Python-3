0e265bf8c45052487b49f643e8589831e2269fc2
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-08-02 13:55:27 -0300

    Update ex097.py

--- exercicios-python/ex097.py
+++ exercicios-python/ex097.py
@@ -1,7 +1,7 @@
 def escreva(txt):
-    print('~' * (len(txt) + 2))
-    print(f' {txt}')
-    print('~' * (len(txt) + 2))
+    print('~' * (len(txt) + 4))
+    print(f'  {txt}')
+    print('~' * (len(txt) + 4))
 
 
 # Programa Principal
