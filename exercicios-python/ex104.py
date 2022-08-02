d8b91cb4e7d30f3b71d4927ef96aaa9765129219
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-08-02 16:56:56 -0300

    Create ex104.py

---
+++ exercicios-python/ex104.py
@@ -0,0 +1,15 @@
+def leiaInt(num):
+    show = False
+    while not show:
+        x = input(num)
+        if not x.isnumeric():
+            print('\033[1;31mERRO! Digite um número válido.\033[m')
+        else:
+            num = int(x)
+            show = True
+    return num
+
+
+# Programa Principal
+n = leiaInt('Digite um número: ')
+print(f'Você acabou de digitar, o número {n}')
