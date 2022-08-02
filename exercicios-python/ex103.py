6080d0ff0e12b254758b07b3f1a83020840cc56f
Vinicius Andrade de Moura <108092099+ViniciusAndradex@users.noreply.github.com>
2022-08-02 16:21:57 -0300

    Create ex103.py

---
+++ exercicios-python/ex103.py
@@ -0,0 +1,15 @@
+def ficha(jog='', gols=0):
+    if jog == '':
+        jog = '<desconhecido>'
+    return f'O jogador {jog} fez {gols} gol(s) no campeonato.'
+
+
+# Programa Principal
+print('=' * 10)
+jogador = str(input('Nome do Jogador: ')).strip()
+gol = str(input('Número de Gols: ')).strip()
+if gol == '':
+    gol = int(0)
+else:
+    gol = int(gol)
+print(ficha())
