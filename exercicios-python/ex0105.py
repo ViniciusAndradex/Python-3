7b69ab2a38a9d603bcde41dad4310c4c86be535c
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-08-03 10:32:00 -0300

    Update ex0105.py

--- exercicios-python/ex0105.py
+++ exercicios-python/ex0105.py
@@ -1,6 +1,11 @@
 def nota(* notas, situacao=False):
+    """
+    -> Função para analisar notas e situações de vários alunos.
+    :param notas: uma ou mais notas dos alunos (aceita várias notas).
+    :param situacao: valor opcional, indicando sobre a situação da turma.
+    :return: dicinário com várias informações sobre a situação da turma.
+    """
     media = 0
-    sit = ''
     aluno = dict()
     aluno['total'] = len(notas)
     aluno['maior'] = max(notas)
@@ -24,3 +29,4 @@ resp = nota(3.5, 10, 6.5)
 resp1 = nota(3.5, 2, 6.5, 2, 7, 4, situacao=True)
 print(resp)
 print(resp1)
+help(nota)
