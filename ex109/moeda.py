bf39f3c3bd9189e6e90fd146a5d85e95e72da40f
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-08-03 15:45:17 -0300

    create

---
+++ ex109/moeda.py
@@ -0,0 +1,32 @@
+def metade(num, show=False):
+    valor = num / 2
+    if show:
+        valor = moeda(valor)
+    return
+
+
+def dobro(num, show=False):
+    valor = num * 2
+    if show:
+        valor = moeda(valor)
+    return valor
+
+
+def aumentar(num, perc, show=False):
+    valor = num + (num * perc / 100)
+    if show:
+        valor = moeda(valor)
+    return valor
+
+
+def diminuir(num, perc, show=False):
+    valor = num - (num * perc / 100)
+    if show:
+        valor = moeda(valor)
+    return valor
+
+
+def moeda(num):
+    formatacao = f'R${num:.2f}'
+    formatacao = formatacao.replace('.', ',')
+    return formatacao
