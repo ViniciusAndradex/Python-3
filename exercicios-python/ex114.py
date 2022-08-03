b5e035a267b34ef16cc4b4cb91649026f6b1ea04
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-08-03 20:17:33 -0300

    update

---
+++ exercicios-python/ex114.py
@@ -0,0 +1,9 @@
+from urllib import request, error
+try:
+    site = request.urlopen('http://www.pudim.com.br')
+except error.URLError:
+    print('O site Pudim não está acessível no momento')
+else:
+    print('Consegui acessar o site pudim com sucesso')
+    print(site.read())
+    
