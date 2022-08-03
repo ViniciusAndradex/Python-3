4dd29d6b08d4c3b376d499d54825a21d28e27c45
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-08-03 15:06:59 -0300

    add

---
+++ ex107/moeda/__init__.py
@@ -0,0 +1,14 @@
+def metade(num):
+    return num / 2
+
+
+def dobro(num):
+    return num * 2
+
+
+def aumentar(num, perc):
+    return num + (num * perc / 100)
+
+
+def diminuir(num, perc):
+    return num - (num * perc / 100)
