ec41ed6ae79511d2c540b563ab20a7822ddda4ed
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-08-03 19:48:24 -0300

    update

--- exercicios-python/ex113.py
+++ exercicios-python/ex113.py
@@ -1,2 +1,4 @@
-from uteis.numeros import leiaInt
+from uteis.numeros import leiaInt, leiaReal
 inteiro = leiaInt('Digite um número: ')
+real = leiaReal('Digite um Real: ')
+print(f'O valor inteiro digitado foi {inteiro} eo real foi {real}')
--- uteis/numeros/__init__.py
+++ uteis/numeros/__init__.py
@@ -17,7 +17,14 @@ def leiaInt(num):
     while True:
         try:
             x = int(input(num))
-            x = int(x)
             return x
-        except Exception as erro:
-            print(f'\033[1;31m{erro.}: por favor, digite um número inteiro válido.\033[m')
+        except ValueError:
+            print(f'\033[1;31m{ValueError}: por favor, digite um número inteiro válido.\033[m')
+
+def leiaReal(num):
+    while True:
+        try:
+            x = float(input(num))
+            return x
+        except ValueError:
+            print(f'\033[1;31m{ValueError}: por favor, digite um número real válido.\033[m')
