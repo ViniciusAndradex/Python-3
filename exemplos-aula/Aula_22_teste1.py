05a72e8d4517be4d7ababe1765e39de0772ae3c5
Vinicius Andrade de Moura <viniciusandradeprog@gmail.com>
2022-08-03 14:50:39 -0300

    update and create packages

---
+++ exemplos-aula/Aula_22_teste1.py
@@ -0,0 +1,5 @@
+from uteis import numeros
+num = int(input('Digite um valro: '))
+fat = numeros.fatorial(num)
+print(f'O fatorial de {num} é {fat}')
+print(f'o dobro de {num} é {numeros.dobro(num)}')
